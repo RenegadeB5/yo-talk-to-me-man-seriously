cd3cc26456ea8127843e77a23a542ee8ae13b63b
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-03 15:37:31 -0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,11 @@
-        client.users.get(dmsend).send(((reaction.message.embeds).map(r => r.image))[0])
+async function links(userid) {
+      const response = await linkdb_channel.fetchMessages()
+      const linksend = response.map(r => r.content)
+      var linkcount = 0
+      linksend.forEach(function (message) {
+          if (((message.split(" "))[0]) === userid) {
+              linkcount += 1
+              }
+          });
+      return linkcount
+  }
